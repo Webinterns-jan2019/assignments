1f4fb56455fb726f2fadce7f30a444bd0338c05d
sushmithasu <46806695+sushmithasu@users.noreply.github.com>
2019-01-22 15:03:20 +0530

    added files

---
+++ demo.js
@@ -0,0 +1,25 @@
+function demo(){
+	var a='HPE MANGALORE'
+		console.log(a.substring(2,5));
+		console.log(a);
+		
+	var b="HPE MANGALORE'
+		console.log(b.split(''));
+		
+		var c=["HTML","CSS","JS","PHP","ReactJS","NodeJS"];
+			console.log(c.reverse());
+			
+		var a=["H","P","E","","M","A","N","G","A","L","O","R","E"]
+		console.log(a.join(''));
+		
+		var a='				HPE MANGALORE		';
+		console.log(a.trim());
+		
+		var a=["HTML","CSS","JS","PHP","ReactJS","NodeJS"];
+		console.log(a.toUpperCase());
+		console.log(ba.toLowerCase());
+		
+		var a='HPE MANGALORE';
+		console.log(a.charAt(0));
+		}
+		
