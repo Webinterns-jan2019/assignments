053e6414e6b89208dd67912744d718d88bd6da95
sushmithasu <46806695+sushmithasu@users.noreply.github.com>
2019-01-22 14:57:08 +0530

    added files

---
+++ clock.js
@@ -0,0 +1,19 @@
+body{
+    background:#000;
+}
+#clock{
+    background:purple;
+    color:yellow;
+    font-family:sans-Serif;
+    font-size:7.125em;
+    margin:auto;
+    padding:0.5em 0em;
+    text-align:center;
+    text-Transform: uppercase;
+    width: 80%;
+}
+#not clock{
+    background:orange;
+    color:white;
+    
+}
