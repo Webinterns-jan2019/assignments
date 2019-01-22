49e840a3695be7728f4673234a84b85b302c505b
sushmithasu <46806695+sushmithasu@users.noreply.github.com>
2019-01-22 15:08:14 +0530

    added files

---
+++ function2.js
@@ -0,0 +1,11 @@
+function add(){
+					var a=["HTML","CSS","JS","PHP","ReactJS","NodeJS"];
+					console.log("slice a",a.slice(2,4));//{"JS","PHP"};
+					console.log("a",a);//["HTML","CSS","JS","PHP","ReactJS","NodeJS"]
+					
+					var b=["HTML","CSS","JS","PHP","ReactJS","NodeJS"];
+					console.log("slice b",b.slice(2,2));//{"JS","PHP"};
+					console.log("b",b);//["HTML","CSS","JS","PHP","ReactJS","NodeJS"]
+					
+					}
+				
