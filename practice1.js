36dc0fd3b29b40c2dac9aa643cf940aeae8f4cb6
sushmithasu <46806695+sushmithasu@users.noreply.github.com>
2019-01-22 14:38:15 +0530

    added files

---
+++ practice1.js
@@ -0,0 +1,32 @@
+function add(){
+	var a=5;
+	var b=3;
+	document.write(a+b);
+}
+
+function println(){
+	for(var a=1;a<=10;a++)
+	document.write(a);
+}
+
+function printeven(){
+	for(var a=11;a<=21;a++)
+		if(a%2==0)
+	document.write(a);
+}
+
+function printodd(){
+	for(var a=10;a<=22;a++)
+	if(a%2!=0)
+	document.write(a);
+}
+
+function palindrome(){
+	var a="hello"
+	var b=a.reverse();
+	document.write(b);
+}
+
+	
+
+	
